38365ee825a82e454a344d2d0c3f3898b884306a
Adrien PELFRESNE <adrienpelfresne@MacBook-Pro-de-Adrien.local>
2023-01-25 10:57:09 +0100

    battle class update

--- src/AivsAi.py
+++ src/AivsAi.py
@@ -1,13 +1,13 @@
 from abc import ABC, abstractmethod
 from aiproject.mcts.node import Node
 from aiproject.mcts.search import Search
-from typing import Self
 import numpy as np
 import aiproject.mc.monte_carlo as mc
 import aiproject.common.impl as common
 
 STARTING_BOARD = np.zeros(144, dtype=np.uint8)
 common._update_possible_moves(STARTING_BOARD, 0)
+
 
 class Player(ABC):
     def __init__(self) -> None:
@@ -22,8 +22,13 @@ class Player(ABC):
         """
         pass
 
-    def play(self, board: np.ndarray, move_id: int) -> None:
-        common.play_one_turn(board, move_id)
+    def play(self, board: np.ndarray) -> None:
+        move_id: int = self.find_best_move()
+        play_id: int = board[move_id]
+        common.play_one_turn(board, play_id)
+
+    def __str__(self) -> str:
+        return __class__.__name__
 
 
 class MonteCarloPlayer(Player):
@@ -52,13 +57,27 @@ class Battle:
     """
 
     def __init__(
-        self, player_1: Player, player_2: Player, number_of_match: int = 10
+        self, player_0: Player, player_1: Player, number_of_match: int = 10
     ) -> None:
         self.number_of_match = number_of_match
+        self.player_0 = player_0
         self.player_1 = player_1
-        self.player_2 = player_2
-        self.result = []
+        self.current_player = 0
+        self.results = {}
 
-    def battle() -> None:
-        board: np.ndarray = common.START
+    def get_current_player_instance(self) -> Player:
+        if self.current_player == 0:
+            return self.player_0
+        return self.player_1
 
+    def get_player_instance_by_result(self, result: int) -> Player:
+        if result == 1:
+            return self.player_0
+        return self.player_1
+
+    def battle(self) -> None:
+        board: np.ndarray = STARTING_BOARD
+        while not common.is_over(board):
+            self.get_current_player_instance().play(board)
+        result: int = common.get_winner(board)
+        self.results[str(self.get_player_instance_by_result(result))] += 1
